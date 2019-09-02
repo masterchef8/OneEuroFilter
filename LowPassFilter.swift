65dda9e530fc1b69bff136a4bacc2a118dd0d42e
GIRAUDEAU Philippe <philippe.giraudeau@inria.fr>
2019-09-03 00:58:53 +0200

    add source files

---
+++ LowPassFilter.swift
@@ -0,0 +1,54 @@
+//
+//  OneEuroFilter.swift
+//
+//  Created by Giraudeau Philippe @ Inria Bordeaux Sud-Ouest, France on 02/09/2019.
+//  Copyright © 2019 Inria Bordeaux Sud-Ouest. All rights reserved.
+//
+
+import Foundation
+
+public class LowPassFilter {
+    var y: Double
+    var a: Double = 0.0
+    var s: Double
+    var initialized: Bool
+    
+    init(alpha:Double, initval:Double = 0.0) {
+        y = initval; s = initval
+        initialized = false
+        setAlpha(alpha: alpha)
+    }
+    
+    public func setAlpha(alpha: Double){
+        if ((alpha <= 0.0) || (alpha > 1.0)) {
+            print("Alpha should be in range [0.0..1.0]!alpha=%lf",alpha)
+        } else {
+            self.a = alpha }
+    }
+    
+    public func filter(value:Double) -> Double {
+        var result:Double
+        if (initialized){
+        result = a * value + (1.0 - a) * s
+        }else {
+            result = value
+            initialized = true
+        }
+        y = value
+        s = result
+        return result;
+    }
+    
+    public func filterWithAlpha(value:Double, alpha:Double) -> Double {
+        setAlpha(alpha: alpha)
+        return filter(value: value)
+    }
+    
+    public func hasLastRawValue() -> Bool {
+        return initialized
+    }
+    
+    public func lastRawValue() -> Double {
+        return y
+    }
+}
---
+++ OneEuroFilter.swift
@@ -0,0 +1,112 @@
+//
+//  OneEuroFilter.swift
+//  DummyOpenCV-ARkit-project
+//
+//  Created by Somebody on 02/09/2019.
+//  Copyright © 2019 Somebody. All rights reserved.
+//
+
+import Foundation
+
+public class OneEuroFilter {
+    var freq: Double
+    var mincutoff: Double
+    var beta:Double
+    var dcutoff:Double
+    var x:LowPassFilter
+    var dx:LowPassFilter
+    var lasttime:Double
+    
+    var currValue:Double
+    var prevValue:Double
+    
+    init(freq:Double,  mincutoff:Double = 1.0,  beta:Double = 0.0,  dcutoff:Double = 1.0) {
+        
+        setFrequency(freq);
+        setMinCutoff(mincutoff);
+        setBeta(beta);
+        setDerivateCutoff(dcutoff);
+        
+        x = LowPassFilter(alpha: alpha(mincutoff))
+        dx = LowPassFilter(alpha: alpha(dcutoff))
+        lasttime = -1.0;
+        
+        currValue = 0.0
+        prevValue = currValue
+    }
+    
+    func alpha(_ cutoff:Double) -> Double {
+        let te:Double = 1.0 / freq
+        let tau:Double = 1.0 / (2.0 * Double.pi * cutoff)
+        return 1.0 / (1.0 + tau / te)
+    }
+    
+    func setFrequency(_ f:Double) {
+        if (f <= 0.0) {
+            print("freq =" ,f, ". It should be > 0")
+            return
+        }
+        freq = f
+    }
+    
+    func setMinCutoff(_ mc:Double)
+    {
+        if (mc <= 0.0){
+            print("mincutoff should be > 0");
+            return
+        }
+        mincutoff = mc
+    }
+    
+    func setBeta(_ b:Double)
+    {
+        beta = b
+    }
+    
+    func setDerivateCutoff(_ dc:Double)
+    {
+        if (dc <= 0.0){
+            print("dcutoff should be > 0")
+            return
+        }
+        dcutoff = dc
+    }
+    
+    public func UpdateParams(_ freq:Double, _ mincutoff:Double, _ beta:Double, _ dcutoff:Double){
+        setFrequency(freq)
+        setMinCutoff(mincutoff)
+        setBeta(beta)
+        setDerivateCutoff(dcutoff)
+        x.setAlpha(alpha: alpha(mincutoff))
+        dx.setAlpha(alpha: alpha(dcutoff))
+    }
+    
+    public func UpdateParams(freq: Double, mincutoff: Double, beta: Double, dcutoff: Double)
+    {
+        setFrequency(freq)
+        setMinCutoff(mincutoff)
+        setBeta(beta)
+        setDerivateCutoff(dcutoff)
+        x.setAlpha(alpha: alpha(mincutoff))
+        dx.setAlpha(alpha: alpha(dcutoff))
+    }
+    
+    public func Filter(_ value, timestamp = -1.0) -> Double {
+        prevValue = currValue
+    
+        // update the sampling frequency based on timestamps
+        if (lasttime != -1.0 && timestamp != -1.0){
+            freq = 1.0 / (timestamp - lasttime)
+        }
+        lasttime = timestamp
+        // estimate the current variation per second
+        var dvalue = x.hasLastRawValue() ? (value - x.lastRawValue()) * freq : 0.0 // FIXME: 0.0 or value?
+        var edvalue = dx.filterWithAlpha(dvalue, alpha(dcutoff))
+        // use it to update the cutoff frequency
+        var cutoff = mincutoff + beta * Double.Abs(edvalue)
+        // filter the given value
+        currValue = x.filterWithAlpha(value, alpha(cutoff))
+    
+        return currValue
+    }
+}
